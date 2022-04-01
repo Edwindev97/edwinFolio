14e2a344135cb84db4c4feda5121cda716d19ffa
Edwin Raj <edwin.raj@zibtek.in>
2022-04-01 11:55:15 +0530

    remove name

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Edwin  Raj",
+  username: "  Raj",
   title: "Hi all, I'm Raj",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
